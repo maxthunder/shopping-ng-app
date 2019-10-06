a9cafa2987d49d776a2f299d6003cdfe2a5d6a2d
maxthunder <maxwell.stark@gmail.com>
2019-10-06 01:40:22 -0600

    add buildspec

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: [ './app.component.css' ]
 })
-export class AppComponent {
-  title = 'first';
-}
+export class AppComponent  {}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
